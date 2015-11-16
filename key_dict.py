ee3170198dfeb0267dad1884d04be53deca75bc4
umaaaaa <k-kurakata@hykwlab.org>
2015-11-17 01:01:30 +0900

    key_dictバイバイ

--- key_dict.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-oath_key_dict = {
-        "consumer_key": "i2hSJn4bgO0szcZ9pYNITJo9o",
-        "consumer_secret": "vSUAaK7WrpwIaR8CWorO3Sm6TbVAes5JG4vLHggBEh2dte0Tvn",
-        "access_token": "120988585-0XrARE7HebKzaZM7o8bqIbvNqJbXn7vah3J3kDwX",
-        "access_token_secret": "1wHmO7ngtxZ7qMrUW3Lo1AjrBJmB37ritBN2NNbMqHV22"
-}
